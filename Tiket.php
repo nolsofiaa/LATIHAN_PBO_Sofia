919ac8c73171e935f44403804e3b22b4d8612dbc
nolsofiaa <nolsofiaa93@gmqail.com>
2026-06-17 13:08:02 +0700

    [tahap 3] Membuat abstract class dan koneksi database

---
+++ Tiket.php
@@ -0,0 +1,27 @@
+<?php
+abstract class Tiket {
+    protected $id_tiket;
+    protected $nama_film;
+    protected $jadwal_tayang;
+    protected $jumlah_kursi;
+    protected $hargaDasarTiket;
+
+    public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket) {
+        $this->id_tiket = $id_tiket;
+        $this->nama_film = $nama_film;
+        $this->jadwal_tayang = $jadwal_tayang;
+        $this->jumlah_kursi = $jumlah_kursi;
+        $this->hargaDasarTiket = $hargaDasarTiket;
+    }
+
+    // Getter methods
+    public function getIdTiket() { return $this->id_tiket; }
+    public function getNamaFilm() { return $this->nama_film; }
+    public function getJadwal() { return $this->jadwal_tayang; }
+    public function getJumlahKursi() { return $this->jumlah_kursi; }
+    public function getHargaDasar() { return $this->hargaDasarTiket; }
+
+    abstract public function hitungTotalHarga();
+    abstract public function tampilkanInfoFasilitas();
+}
+?>
---
+++ TiketRegular.php
@@ -0,0 +1,25 @@
+<?php
+require_once 'Tiket.php';
+
+class TiketRegular extends Tiket {
+    private $tipeAudio;
+    private $lokasiBaris;
+
+    public function __construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket, $tipeAudio, $lokasiBaris) {
+        parent::__construct($id_tiket, $nama_film, $jadwal_tayang, $jumlah_kursi, $hargaDasarTiket);
+        $this->tipeAudio = $tipeAudio;
+        $this->lokasiBaris = $lokasiBaris;
+    }
+
+    public function getTipeAudio() { return $this->tipeAudio; }
+    public function getLokasiBaris() { return $this->lokasiBaris; }
+
+    public function hitungTotalHarga() {
+        return $this->jumlah_kursi * $this->hargaDasarTiket;
+    }
+
+    public function tampilkanInfoFasilitas() {
+        return "Tipe Audio: {$this->tipeAudio}, Lokasi Baris: {$this->lokasiBaris}";
+    }
+}
+?>
